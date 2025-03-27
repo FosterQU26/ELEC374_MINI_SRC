3c7527b76b9778e779b7524d696e243d086e1b75
Mattias Zeni <mattias.zeni@gmail.com>
2025-03-27 11:15:35 -0400

    Phase 4 Edits

---
+++ input_sanitizer.v
@@ -0,0 +1,73 @@
+
+/*
+	input_sanitizer is an FSM that regulates an input signal by interpreting a constant logic 1 as a single signal pulse, either at the start of end of signal assertion.
+	It accepts 1-bit input in, and asserts the single pulse signal out_start when the input goes high, and asserts out_end when the input goes low. 
+	The FSM is synchronized to clk with a synch. reset.
+*/
+
+module input_sanitizer (
+	input clk, reset, in,
+	output out
+);
+	
+	
+	// There are two states: OFF (0) and ON (1)
+	reg ps, ns;
+	
+	// Next state logic (see state diagram in report)
+	always begin
+		case (ps)
+			1'b0:	if (in) ns = 1'b1;
+				else ns = 1'b0;
+			1'b1: 	if (in) ns = 1'b1;
+				else ns = 1'b0;
+		endcase
+	end
+	
+	// asserting outputs.
+	assign out = (ps == 1'b0) & in & ~reset; // When input goes high
+	
+	// State transitions.
+	always @(posedge clk) begin
+		if (reset)
+			ps <= 1'b0;
+		else 
+			ps <= ns;
+	end
+endmodule 
+
+
+// This testbench verifies that for a sustained assertion of in, out_start pulses for a single clock cycle at the start of the assertion,
+// and out_end pulse for a single clock cycle at the end of the assertion.
+
+module input_sanitizer_testbench();
+	reg clk, reset, in; 
+	wire out;
+	
+	// Above design under test.
+	input_sanitizer dut (clk, reset, in, out);
+	
+	// Establishing clock behaviour.
+	parameter clock_period = 20;
+	initial begin
+		clk <= 0;
+		forever #(clock_period/2) clk <= ~clk;
+	end
+	
+	initial begin
+		reset <= 1;	in <= 0;	@(posedge clk)
+		reset <= 0;	in <= 1;	@(posedge clk)	
+		
+		// Hold in high for 10 cycles.
+		repeat(10) 
+			@(posedge clk);
+		
+		// Goes low for 10 cycles.
+		in <= 0;					@(posedge clk);
+		
+		repeat(10) 
+			@(posedge clk);
+
+		$stop;
+	end
+endmodule
--- mini_src_group_1.v
+++ mini_src_group_1.v
@@ -1,14 +1,22 @@
 
 module mini_src_group_1 (
-	input clk, reset, stop,
-	input [31:0] INPORTin,
-	output [31:0] OUTPORTout,
-	output run
+	input CLOCK_50, 
+	input [1:0] KEY,
+	input [7:0] SW,
+	output [6:0] HEX0, HEX1,
+	output [5:5] LEDR
 	);
 	
 	// Program Execution Control
-	wire clr, CONin, CON, RAM_wr;
+	wire clr, CONin, CON, RAM_wr, reset, stop;
+	wire [31:0] INPORTin = {24'b0, SW};
+	wire [31:0] OUTPORTout;
 	wire [31:27] IRop;
+	
+	input_sanitizer sanitized_reset (CLOCK_50, 1'b0, ~KEY[0], reset);
+	input_sanitizer sanitized_stop (CLOCK_50, 1'b0, ~KEY[1], stop);
+	hex_to_7 out3_0 (OUTPORTout[3:0], HEX0);
+	hex_to_7 out7_4 (OUTPORTout[7:4], HEX1);
 	
 	// General Purpose Register Control
 	wire Gra, Grb, Grc, Rin, Rout, BAout;
@@ -17,13 +25,11 @@ module mini_src_group_1 (
 	wire [15:0] DPin, DPout;
 	
 	// ALU Control
-	
 	wire [15:0] ALUopp;
 	
-	
-	DataPath DP (clk, clr, CONin, Gra, Grb, Grc, Rin, Rout, BAout, RAM_wr, DPin, DPout, ALUopp, INPORTin, OUTPORTout, IRop, CON);
+	DataPath DP (CLOCK_50, clr, CONin, Gra, Grb, Grc, Rin, Rout, BAout, RAM_wr, DPin, DPout, ALUopp, INPORTin, OUTPORTout, IRop, CON);
 
-	Control ctrl (reset, stop, clk, CON, IRop, clr, CONin, RAM_wr, Gra, Grb, Grc, Rin, Rout, BAout, DPin, DPout, ALUopp, run);
+	Control ctrl (reset, stop, CLOCK_50, CON, IRop, clr, CONin, RAM_wr, Gra, Grb, Grc, Rin, Rout, BAout, DPin, DPout, ALUopp, LEDR[5]);
 	
 endmodule
 
@@ -35,7 +41,7 @@ module tl_testbench();
 	wire [31:0] OUTPORTout;
 	wire run;
 	
-	mini_src_group_1 uut (clk, reset, stop, INPORTin, OUTPUTout, run);
+	mini_src_group_1 uut (clk, reset, stop, INPORTin, OUTPORTout, run);
 	
 	initial begin
 		clk <= 0;
